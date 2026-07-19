ef0d04490726518d8984af97b7c4eed99515cd28
Fl1riX <maksimhripunkov658@gmail.com>
2026-07-19 16:49:56 +0300

    feat(core): Add a function to count a cell's neighbors

---
+++ src/core/life.c
@@ -0,0 +1,20 @@
+#include "life.h"
+
+int life_count_neighbours(
+    int field[FIELD_ROWS][FIELD_COLS], 
+    int row, 
+    int col
+) {
+    int result = 0;
+    for (int i = row - 1; i < ROWS; i++) {
+        for (int j = col - 1; j < COLS; j++) {
+            // (i + ROWS) % ROWS - формула вычислений координаты соседа, которая 
+            // не позволяет выйти за границы массива
+            int current_cell = field[(i + ROWS) % ROWS][j + COLS] % COLS;
+            if (current_cell == 1 && (i != row || j != col)) {
+                result++;
+            }
+        }
+    }
+    return result;
+}
---
+++ src/core/life.h
@@ -0,0 +1,14 @@
+#ifndef LIFE_H
+#define LIFE_H
+
+#define ROWS 25
+#define COLS 80
+
+//void life_init(int field[FIELD_ROWS][FIELD_COLS]);
+int life_count_neighbours(
+    int field[FIELD_ROWS][FIELD_COLS], 
+    int row, 
+    int col
+)
+
+#endif
