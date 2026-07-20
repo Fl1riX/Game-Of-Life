2a1dd9ef441ba5eb79bfd47d77b09772986bcfa5
Fl1riX <maksimhripunkov658@gmail.com>
2026-07-20 09:42:40 +0300

    feat(core): Add a function to calculate the field state for the next game tick

--- src/core/life.c
+++ src/core/life.c
@@ -1,6 +1,6 @@
 #include "life.h"
 
-void life_next_state(int current_state, int neighbours) {
+int life_next_state(int current_state, int neighbours) {
     int result = 0;
     
     if (current_state == 1) {
@@ -32,4 +32,23 @@ int life_count_neighbours(
         }
     }
     return result;
+}
+
+void life_next_generation(
+    int field[FIELD_ROWS][FIELD_COLS], 
+    int next[FIELD_ROWS][FIELD_COLS]
+) {
+    for (int row = 0; row < ROWS; row++) {
+        for (int col = 0; col < COLS; col++) {
+            if (life_next_state(field[row][col], life_count_neighbours(
+                field,
+                row,
+                col
+            )) == 1) {
+                next[row][col] = 1;
+            } else {
+                next[row][col] = 0;
+            }
+        }
+    }
 }
--- src/core/life.h
+++ src/core/life.h
@@ -4,11 +4,15 @@
 #define ROWS 25
 #define COLS 80
 
-void life_next_state(int current_state, int neighbours);
+int life_next_state(int current_state, int neighbours);
 int life_count_neighbours(
     int field[FIELD_ROWS][FIELD_COLS], 
     int row, 
     int col
-)
+);
+void life_next_generation(
+    int field[FIELD_ROWS][FIELD_COLS], 
+    int next[FIELD_ROWS][FIELD_COLS]
+);
 
 #endif
