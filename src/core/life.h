a6905dc8ee61b8b512d8a7b1a68bfb459d9448f4
Fl1riX <maksimhripunkov658@gmail.com>
2026-07-30 21:20:59 +0300

    fix(core): fix the data type mismatch between the next-state calculation function and its prototype

--- src/core/life.h
+++ src/core/life.h
@@ -9,7 +9,7 @@
 // Создаем константы через которые будем присваивать значение
 typedef enum { CELL_DEAD = 0, CELL_ALIVE = 1 } CellState;
 
-int life_next_state(int current_state, int neighbours);
+CellState life_next_state(CellState current_state, int neighbours);
 int life_count_neighbours(const int field[FIELD_ROWS][FIELD_COLS], int row,
                           int col);
 void life_next_generation(const int field[FIELD_ROWS][FIELD_COLS],
